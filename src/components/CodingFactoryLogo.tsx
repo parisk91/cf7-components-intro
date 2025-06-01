a27b4799297f99066ea341dd415ff96d25ef48ea
parisk91 <paris_k91@hotmail.com>
2025-06-01 22:02:09 +0300

    feat: add cf logo

---
+++ src/components/CodingFactoryLogo.tsx
@@ -0,0 +1,12 @@
+const CodingFactoryLogo = () => {
+    return (
+        <>
+            <img
+                className="my-4 h-16"
+                src="/CFlogo.png"
+                alt="CF7"/>
+        </>
+    )
+}
+
+export default CodingFactoryLogo
